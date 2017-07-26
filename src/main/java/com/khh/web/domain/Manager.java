cf72b084e8516e27137081bc70ba704b0e3e2dc6
kuanghh <951087952@qq.com>
2017-07-26 10:15:18 +0800

    new

---
+++ src/main/java/com/khh/web/domain/Manager.java
@@ -0,0 +1,43 @@
+package com.khh.web.domain;
+
+public class Manager {
+    private Integer id;
+
+    private String name;
+
+    private String interest;
+
+    private Integer deptId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest == null ? null : interest.trim();
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+}
